687acf3046abfd763d75f0c20b572d0190c59402
babnbabn <48063718+babnbabn@users.noreply.github.com>
2019-05-25 15:11:11 +0900

    Create 4673.cpp

---
+++ 4673.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int d(int n) {
+	if (n == 0)return 0;
+	else {
+		return d(n / 10) + n % 10;
+	}
+}
+int main() {
+	int A[100000] = {};
+	int i;
+	for (i = 1; i <= 9972; i++) {
+		int a = d(i);
+		A[a+ i] = 1;
+	}
+	
+	for (i = 1; i < 10000; i++) {
+		if (A[i]!=1) {
+			printf("%d\n", i);
+		}
+	}
+}
